9e91a7fa6c431d31c16e780f21d4738bb3b0bd82
00zkr <zakariarhendour@gmail.com>
2025-12-08 07:05:28 +0100

    first commit

--- Centre_Medical_db.sql
+++ Centre_Medical_db.sql
@@ -43,13 +43,13 @@ VALUES
 ('Hamza mizo', 'pathologique', '062122232425');
 
 -- Creation du table Rendez-vous
-CREATE TABLE RDV(
-	idRDV INT auto_increment PRIMARY KEY,
+CREATE TABLE RDV (
     dateRDV DATE,
-    acte VARCHAR(70),
+    acte VARCHAR(255),
     tarif DOUBLE,
-    idPt INT,
+	idPt INT,
     idMd INT,
+    PRIMARY KEY (idPt, idMd, dateRDV),
     FOREIGN KEY (idPt) REFERENCES Patient(idPt),
     FOREIGN KEY (idMd) REFERENCES Medcin(idMd)
 );
@@ -68,3 +68,33 @@ CREATE TABLE User (
     nom VARCHAR(50),
     password CHAR(64) NOT NULL
 );
+
+-- +7 rows for Patient (total 10)
+INSERT INTO Patient(nom, age, ville) VALUES
+('Drake Graham', 39, 'Toronto'),
+('Rihanna Fenty', 37, 'Bridgetown'),
+('The Weeknd', 35, 'Toronto'),
+('Adele Adkins', 36, 'London'),
+('Bruno Mars', 40, 'Honolulu'),
+('Doja Cat', 29, 'Los Angeles'),
+('Billie Eilish', 23, 'Los Angeles');
+
+-- +7 rows for Medcin (total 10)
+INSERT INTO Medcin(nom, specialite, telephone) VALUES
+('Youssef Karim', 'Dermatologie', '0601010101'),
+('Amina Zahra', 'Pediatrie', '0602020202'),
+('Omar Benali', 'Orthopedie', '0603030303'),
+('Sara Elhadi', 'Gynecologie', '0604040404'),
+('Nabil Farouk', 'Ophtalmologie', '0605050505'),
+('Imane Toumi', 'Psychiatrie', '0606060606'),
+('Hakim Razi', 'Radiologie', '0607070707');
+
+-- +7 rows for RDV (total 10)
+INSERT INTO RDV(dateRDV, acte, tarif, idPt, idMd) VALUES
+('2025-12-01', 'Consultation', 120, 1, 3),
+('2025-12-03', 'Analyse', 200, 10, 2),
+('2025-12-05', 'Consultation', 150, 12, 8),
+('2025-12-06', 'IRM', 500, 14, 10),
+('2025-12-07', 'Consultation', 130, 12, 14),
+('2025-12-08', 'Radio', 280, 15, 9),
+('2025-12-09', 'Controle', 100, 10, 9);
